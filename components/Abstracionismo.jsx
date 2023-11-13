1144718a39ae90958890b25f763b842520f1f448
Gabrielly Isadora <isadora.gv10@gmail.com>
2023-11-13 15:39:32 -0300

    outras alterações

--- components/Abstracionismo.jsx
+++ components/Abstracionismo.jsx
@@ -29,7 +29,7 @@ const ImageGallery = (props) => {
                         className={style.imageThumbnail}
                         onClick={() => openImage(imageUrl)}
                     >
-                        <img src={imageUrl.img} alt={`Image ${index}`} />
+                        <Image src={imageUrl.img} alt={`Image ${index}`} width={300} height={300}/>
 
                         
                     </div>
@@ -44,7 +44,7 @@ const ImageGallery = (props) => {
                         &times;
                     </span>
 
-                    <img src={selectedImage} alt="Selected Image"/>
+                    <Image src={selectedImage} alt="Selected Image" width={300} height={300}/>
 
                     <p className="descricao">{selectedText}</p>
 
