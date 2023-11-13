1612bc9c76662559d7b69bd9ae19a293ae218d92
Gabrielly Isadora <isadora.gv10@gmail.com>
2023-11-13 14:42:53 -0300

    modificação thais

--- app/realismo/page.jsx
+++ app/realismo/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 /*teste*/
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import style from "@/app/realismo/realismo.module.css";
 import Link from "next/link";
 import ImageGallery from "@/components/Realismo";
